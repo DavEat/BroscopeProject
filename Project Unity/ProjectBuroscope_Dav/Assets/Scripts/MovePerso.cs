6effbc359b871bf5e8690f6c269369b3324a711b
DavEat <oned@hotmail.be>
2016-05-29 00:28:06 +0200

    UpDate - Npc

--- Project Unity/ProjectBuroscope_Dav/Assets/Scripts/MovePerso.cs
+++ Project Unity/ProjectBuroscope_Dav/Assets/Scripts/MovePerso.cs
@@ -5,7 +5,6 @@ public class MovePerso : MonoBehaviour {
 
     private NavMeshAgent navA;
 
-
     void Awake()
     {
         navA = GetComponent<NavMeshAgent>();
---
+++ Project Unity/ProjectBuroscope_Dav/Assets/Scripts/NpcMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcMove : MonoBehaviour {
+
+    public Transform listOfOption;
+    public int currentPoint;
+
+    public bool waiting;
+    public float waitingTime;
+
+    private NavMeshAgent navA;
+
+    void Awake()
+    {
+        navA = GetComponent<NavMeshAgent>();
+        currentPoint = Random.Range(0, listOfOption.childCount - 1);
+    }
+
+    void Update()
+    {
+        if (waitingTime <= 0)
+        {
+            if (Vector3.Distance(transform.position, listOfOption.GetChild(currentPoint).position) > navA.radius + navA.radius/3)
+                navA.SetDestination(listOfOption.GetChild(currentPoint).position);
+            else if (Random.value > 0.4f)
+                currentPoint = Random.Range(0, listOfOption.childCount - 1);
+            else waitingTime = Random.Range(1, 10);
+        }
+        else waitingTime -= Time.deltaTime;
+    }
+}
