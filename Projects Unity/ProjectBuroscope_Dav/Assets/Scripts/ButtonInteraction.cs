1a12ac185e6e3477dc95e1cc86204df3e52777a3
DavEat <oned@hotmail.be>
2016-06-16 19:42:45 +0200

    Add - First Part of the menu formation

---
+++ Projects Unity/ProjectBuroscope_Dav/Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ButtonInteraction : MonoBehaviour {
+
+    public static void ButtonEnter (Transform t)
+    {
+        t.localScale += t.localScale / 10;
+    }
+
+    public static void ButtonExit(Transform t)
+    {
+        t.localScale -= t.localScale / 11;
+    }
+
+    public static void ButtonGrowing(Transform t, int scale)
+    {
+    }
+}
---
+++ Projects Unity/ProjectBuroscope_Dav/Assets/Scripts/OnClick.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems; // 1
+
+public class OnClick : MonoBehaviour
+    , IPointerClickHandler // 2
+    , IDragHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
+// ... And many more available!
+{
+    public bool grow;
+    public int speed = 3, buttonId;
+
+    public Vector2 baseScale, targetScale;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        baseScale = transform.localScale;
+
+        anim = transform.parent.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (grow)
+            if (Input.GetMouseButtonDown(1) || Input.touchCount >= 2)
+            {
+                grow = false;
+                anim.Play(string.Concat("animButtonReturn", buttonId));
+            }
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        if (eventData.button == PointerEventData.InputButton.Left && !grow)
+        {
+            grow = true;
+            anim.Play(string.Concat("animButton", buttonId));
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("I'm being dragged! : " + transform.name);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!grow)
+            ButtonInteraction.ButtonEnter(transform);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!grow)
+            ButtonInteraction.ButtonExit(transform);
+    }
+}
